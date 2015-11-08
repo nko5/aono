f156f5b49c537dd2f8f872a0090ce308ec9a4382
Chris Wheatley <chriswheatley87@gmail.com>
2015-11-08 14:21:58 +0000

    write lib added to config file

--- scripts/add.js
+++ scripts/add.js
@@ -1,27 +1,40 @@
 const fs = require('fs')
 const ipfsAPI = require('ipfs-api')
 const rs = require('request-promise')
+const libraries = require('../libraries.json')
 
 const ipfs = ipfsAPI('localhost', '5001')
 
 const source = process.argv[2]
-const sourceSplit = source.split('/')
-const fileName = sourceSplit[sourceSplit.length - 1]
+const sourceSplit = source.split('@')
+const libName = sourceSplit[0].split('/')[3]
+const libVersion = sourceSplit[1].split('/')[0]
 
 rs(source).then((resp) => {
-  fs.writeFile(`/tmp/${fileName}`, resp, (err) => {
-    if(err) {
+  fs.writeFile(`/tmp/${libName}`, resp, (err) => {
+    if (err) {
       return console.log(err)
     }
 
-    ipfs.add(`/tmp/${fileName}`, (err, res) => {
-      if(err || !res) {
+    ipfs.add(`/tmp/${libName}`, (err, res) => {
+      if (err || !res) {
         return console.error(err)
       }
 
       res.forEach((file) => {
-        console.log('hash',file.Hash)
-        console.log('name',file.Name)
+        libraries.push({
+          name: file.Name,
+          version: libVersion,
+          hash: file.Hash
+        })
+
+        fs.writeFile('libraries.json', JSON.stringify(libraries, null, '  '), function (err) {
+          if (err) {
+            return console.error(err)
+          }
+
+          return console.log('written to libraries.json')
+        })
       })
     })
   })
